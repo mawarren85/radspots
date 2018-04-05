1b3e2726ba7f416f0db869d06018385c5d6ae329
Thomas Palzkill <tpalzkilll@gmail.com>
2018-04-05 12:28:46 -0500

    changes

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@
 module.exports = {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       host: '127.0.0.1',
       database: 'radspots',
@@ -35,15 +35,19 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: 'ec2-54-243-213-188.compute-1.amazonaws.com',
+      database: 'dc0ah2gcsj62n4',
+      user:     'kbmvmblzgvdzsk',
+      password: 'c71a66d59fe427ce748992bca944beeeef0cf3a57a1c3449f5f0b030411bdc4b'
     },
     pool: {
       min: 2,
       max: 10
+    }
+    migrations: {
+      tableName: 'knex_migrations'
     }
   }
 
